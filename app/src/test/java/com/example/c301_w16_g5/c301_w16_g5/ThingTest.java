481497ad882d30fd5a893def17c7c8070b289c3a
Shahzeb Asif <asif.shahzeb@hotmail.com>
2016-02-04 11:49:24 -0700

    Wrote some basic use cases and created project.
    Test cases have been created but have nothing inside them.

---
+++ app/src/test/java/com/example/c301_w16_g5/c301_w16_g5/ThingTest.java
@@ -0,0 +1,34 @@
+package com.example.c301_w16_g5.c301_w16_g5;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ThingTest {
+    @Test
+    public void addAThing() {
+    }
+
+    @Test
+    public void SeeAllThings() {
+    }
+
+    @Test
+    public void ViewMyThing() {
+    }
+
+    @Test
+    public void EditMyThing() {
+    }
+
+    @Test
+    public void DeleteMyThing() {
+    }
+
+    @Test
+    public void ThingHasStatus() {
+    }
+}
