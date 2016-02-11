e490d9a3ee22b3ec0472e922ef19303728fe07e1
RobinVerleun <verleun@ualberta.ca>
2016-02-10 21:11:47 -0700

    Borrow, Thing, and Return Test all don't complain. Class Chicken, User done. Activity Login, MainScreen, and ItemView

--- app/src/main/java/com/example/c301_w16_g5/c301_w16_g5/Chicken.java
+++ app/src/main/java/com/example/c301_w16_g5/c301_w16_g5/Chicken.java
@@ -4,18 +4,31 @@ package com.example.c301_w16_g5.c301_w16_g5;
  * Created by Robin on 2016-02-10.
  */
 public class Chicken {
+    public enum Status {
+        AVAILABLE,
+        BORROWED,
+        NOT_AVAILABLE
+    }
 
     private String name;
     private Status status;
-    private String owner;
+    private User owner;
     private String description;
     private double rate;
 
-    public Chicken(){}
+    public Chicken() {
+    }
 
     public Chicken(String name, String description, double rate, Status status) {
         this.name = name;
-        this.owner = owner;
+        this.owner = User.getCurrentUser();
+        this.description = description;
+        this.rate = rate;
+        this.status = status;
+    }
+
+    public void update(String name, String description, double rate, Status status) {
+        this.name = name;
         this.description = description;
         this.rate = rate;
         this.status = status;
@@ -37,11 +50,11 @@ public class Chicken {
         this.status = status;
     }
 
-    public String getOwner() {
+    public User getUser() {
         return owner;
     }
 
-    public void setOwner(String owner) {
+    public void setUser(User owner) {
         this.owner = owner;
     }
 
@@ -60,10 +73,4 @@ public class Chicken {
     public void setRate(double rate) {
         this.rate = rate;
     }
-
-    public enum Status {
-        AVAILABLE, BORROWED, NOT_AVAILABLE
-    }
-
-
 }
