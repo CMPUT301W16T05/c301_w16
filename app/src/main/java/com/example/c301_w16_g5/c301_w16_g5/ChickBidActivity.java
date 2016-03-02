9325fe46e3c997b82bb34023da52493cb87bb85b
Satyen Akolkar <s.akolkar3@gmail.com>
2016-03-02 15:51:26 -0700

    button links

--- app/src/main/java/com/example/c301_w16_g5/c301_w16_g5/ChickBidActivity.java
+++ app/src/main/java/com/example/c301_w16_g5/c301_w16_g5/ChickBidActivity.java
@@ -1,10 +1,12 @@
 package com.example.c301_w16_g5.c301_w16_g5;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public abstract class ChickBidActivity extends AppCompatActivity {
 
@@ -17,5 +19,18 @@ public abstract class ChickBidActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_navbar, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.home_button:
+                startActivity(new Intent(this, HomeActivity.class));
+                break;
+            case R.id.notices_button:
+                // TODO: add next activity to go to
+                break;
+        }
+        return false;
     }
 }
--- app/src/main/java/com/example/c301_w16_g5/c301_w16_g5/HomeActivity.java
+++ app/src/main/java/com/example/c301_w16_g5/c301_w16_g5/HomeActivity.java
@@ -1,7 +1,13 @@
 package com.example.c301_w16_g5.c301_w16_g5;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends ChickBidActivity {
 
@@ -9,8 +15,22 @@ public class HomeActivity extends ChickBidActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
         Toolbar myToolbar = (Toolbar) findViewById(R.id.nav_toolbar);
         setSupportActionBar(myToolbar);
+
+        Button profileButton = (Button) findViewById(R.id.buttonProfile);
+        Button itemsButton = (Button) findViewById(R.id.buttonItems);
+        Button searchButton = (Button) findViewById(R.id.buttonSearch);
+
+        final Intent intent = new Intent(this, AddProfileActivity.class);
+
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -21,7 +41,19 @@ public class HomeActivity extends ChickBidActivity {
 //            }
 //        });
 
-        // disable going back to login screen
-        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        //disable going back to login screen
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+    }
+
+    // TODO: reference source
+    // used to make home icon disappear
+    // http://stackoverflow.com/questions/5440601/android-how-to-enable-disable-option-menu-item-on-button-click answer by Frank
+    // accessed on Mar 2 2016 at 3:20 pm by Satyen Akolkar
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem item = menu.findItem(R.id.home_button);
+        item.setEnabled(false);
+        item.getIcon().setAlpha(0);
+        return true;
     }
 }
