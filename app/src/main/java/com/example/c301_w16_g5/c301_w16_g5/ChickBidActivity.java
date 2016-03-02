8fc4bd668627a801e2247bba0ba4cb892e7143cb
Satyen Akolkar <s.akolkar3@gmail.com>
2016-03-01 19:38:04 -0700

    creating reusable layout

--- app/src/main/java/com/example/c301_w16_g5/c301_w16_g5/ChickBidActivity.java
+++ app/src/main/java/com/example/c301_w16_g5/c301_w16_g5/ChickBidActivity.java
@@ -2,12 +2,29 @@ package com.example.c301_w16_g5.c301_w16_g5;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public abstract class ChickBidActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chick_bid);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // TODO: add main_actionbar to layout
+        getMenuInflater().inflate(R.menu.main_actionbar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        switch(menuItem.getItemId()) {
+            // TODO: add home_button to menu
+            case R.id.home_button:
+                break;
+
+            // TODO: add navigation_button to menu:
+            case R.id.notification_button:
+                break;
+        }
+        return false; // FIXME: return more meaningful result
     }
 }
--- app/src/main/java/com/example/c301_w16_g5/c301_w16_g5/MainScreen.java
+++ app/src/main/java/com/example/c301_w16_g5/c301_w16_g5/MainScreen.java
@@ -7,24 +7,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class MainScreen extends AppCompatActivity {
+public class MainScreen extends ChickBidActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_screen);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main_screen);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//    }
 
 }
