d42dbc96eaaa4aaea80fdade428ad4a17354a8a7
Satyen Akolkar <s.akolkar3@gmail.com>
2016-03-04 11:50:53 -0700

    search extends chickbid activity

--- app/src/main/java/com/example/c301_w16_g5/c301_w16_g5/SearchActivity.java
+++ app/src/main/java/com/example/c301_w16_g5/c301_w16_g5/SearchActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends ChickBidActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
