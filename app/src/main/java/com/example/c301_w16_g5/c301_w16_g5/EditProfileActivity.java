b32b960268595148a43eea2779878523f112f2c3
hailey123 <musselma@ualberta.ca>
2016-03-11 10:51:15 -0700

    really dumb edit profile
    edit profile no longer inherits from user profile

--- app/src/main/java/com/example/c301_w16_g5/c301_w16_g5/EditProfileActivity.java
+++ app/src/main/java/com/example/c301_w16_g5/c301_w16_g5/EditProfileActivity.java
@@ -2,9 +2,9 @@ package com.example.c301_w16_g5.c301_w16_g5;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
 /**
  * This is the view the user sees when they are updating their profile information.
@@ -17,7 +17,9 @@ import android.widget.TextView;
  * @see     UserProfileActivity
  * @see     UserController
  */
-public class EditProfileActivity extends UserProfileActivity {
+public class EditProfileActivity extends ChickBidActivity {
+
+    private User testUser; // TODO: remove
 
     public static final int CREATE_USER_EXTRA_REQ_CODE = 11;
     public static final String CREATE_USER_EXTRA_KEY = "create_user_extra_key";
@@ -25,21 +27,41 @@ public class EditProfileActivity extends UserProfileActivity {
 
     public static final int UPDATE_USER_EXTRA_REQ_CODE = 21;
 
-    private EditText e_usernameTextView;
-    private EditText e_nameTextView;
-    private EditText e_emailTextView;
-    private EditText e_phoneTextView;
-    private EditText e_experienceTextView;
+    private EditText usernameEditText;
+    private EditText nameEditText;
+    private EditText emailEditText;
+    private EditText phoneEditText;
+    private EditText experienceEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_edit_profile);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.nav_toolbar);
-        //setSupportActionBar(toolbar);
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        FloatingActionButton editProfileFAB = (FloatingActionButton) findViewById(R.id.fab);
-        editProfileFAB.setVisibility(View.GONE);
+        setContentView(R.layout.activity_edit_profile);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.nav_toolbar);
+        setSupportActionBar(toolbar);
+
+        // show back arrow at top left
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // TODO: remove
+        testUser = new User("Test_User", "Test", "User", "test@user.com", "1234", "None");
+
+        usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+        nameEditText = (EditText) findViewById(R.id.nameEditText);
+        emailEditText = (EditText) findViewById(R.id.emailEditText);
+        phoneEditText = (EditText) findViewById(R.id.phoneEditText);
+        experienceEditText = (EditText) findViewById(R.id.experienceEditText);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        usernameEditText.setText(testUser.getUsername());
+        nameEditText.setText("Username: " + testUser.getFirstName() + " " + testUser.getLastName());
+        emailEditText.setText("Email: " + testUser.getEmail());
+        phoneEditText.setText("Phone: " + testUser.getPhoneNumber());
+        experienceEditText.setText("Experience: " + testUser.getChickenExperience());
     }
 }
