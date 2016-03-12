a2690f21a87e54fbd3b431002e4b5e467cf8ffbf
Satyen Akolkar <s.akolkar3@gmail.com>
2016-03-12 14:26:49 -0700

    new activity for add chicken

---
+++ app/src/main/java/com/example/c301_w16_g5/c301_w16_g5/AddChickenActivity.java
@@ -0,0 +1,13 @@
+package com.example.c301_w16_g5.c301_w16_g5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddChickenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_chicken);
+    }
+}
