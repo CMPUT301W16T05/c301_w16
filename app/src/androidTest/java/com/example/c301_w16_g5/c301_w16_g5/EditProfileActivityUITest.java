3b310ef2d8ff1e175478c0b5ca3d00c051b136be
hailey123 <musselma@ualberta.ca>
2016-03-12 14:03:03 -0700

    some UI testing

--- app/src/androidTest/java/com/example/c301_w16_g5/c301_w16_g5/EditProfileActivityUITest.java
+++ app/src/androidTest/java/com/example/c301_w16_g5/c301_w16_g5/EditProfileActivityUITest.java
@@ -17,12 +17,27 @@ public class EditProfileActivityUITest extends ActivityInstrumentationTestCase2 
     protected void setUp() throws Exception {
         super.setUp();
         instrumentation = getInstrumentation();
+        ChickBidsApplication.getUserController().setUser(new User("un", "f", "l", "abc@email.com", "780-123-4567", "some"));
         activity = getActivity();
     }
-/*
+
     public void testViewVisible() {
         ViewAsserts.assertOnScreen(activity.getWindow().getDecorView(),
-                activity.findViewById(R.id.toolbar));
+                activity.findViewById(R.id.notifications_button));
+        ViewAsserts.assertOnScreen(activity.getWindow().getDecorView(),
+                activity.findViewById(R.id.home_button));
     }
-*/
+
+    public void testTextVisible() {
+        fail();  // TODO
+    }
+
+    public void testEditProfile() {
+        fail();  // TODO
+    }
+
+    public void testButtonVisible() {
+        ViewAsserts.assertOnScreen(activity.getWindow().getDecorView(),
+                activity.findViewById(R.id.buttonSave));
+    }
 }
--- app/src/androidTest/java/com/example/c301_w16_g5/c301_w16_g5/HomeActivityUITest.java
+++ app/src/androidTest/java/com/example/c301_w16_g5/c301_w16_g5/HomeActivityUITest.java
@@ -1,5 +1,8 @@
 package com.example.c301_w16_g5.c301_w16_g5;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.test.ActivityInstrumentationTestCase2;
 import android.test.ViewAsserts;
 
 /**
@@ -9,11 +12,25 @@ import android.test.ViewAsserts;
  * @version 1.4, 03/07/2016
  * @see     HomeActivity
  */
-/*
-public class HomeActivityUITest extends ToolbarActivityUITest {
+
+public class HomeActivityUITest extends ActivityInstrumentationTestCase2 {
+
+    Instrumentation instrumentation;
+    Activity activity;
 
     public HomeActivityUITest() {
         super(HomeActivity.class);
+    }
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        instrumentation = getInstrumentation();
+        activity = getActivity();
+    }
+
+    public void testViewVisible() {
+        ViewAsserts.assertOnScreen(activity.getWindow().getDecorView(),
+                activity.findViewById(R.id.notifications_button));
     }
 
     public void testButtonsVisible() {
@@ -26,4 +43,4 @@ public class HomeActivityUITest extends ToolbarActivityUITest {
         ViewAsserts.assertOnScreen(activity.getWindow().getDecorView(),
                 activity.findViewById(R.id.buttonSearch));
     }
-}*/
+}
