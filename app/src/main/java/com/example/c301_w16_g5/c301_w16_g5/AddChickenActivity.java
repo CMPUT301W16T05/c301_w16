c8751a335ba87527b3b69fccae72cd43f07b5dd7
Satyen Akolkar <s.akolkar3@gmail.com>
2016-03-13 17:49:49 -0600

    add chicken toolbar

--- app/src/main/java/com/example/c301_w16_g5/c301_w16_g5/AddChickenActivity.java
+++ app/src/main/java/com/example/c301_w16_g5/c301_w16_g5/AddChickenActivity.java
@@ -2,12 +2,19 @@ package com.example.c301_w16_g5.c301_w16_g5;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
-public class AddChickenActivity extends AppCompatActivity {
+public class AddChickenActivity extends ChickBidActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_chicken);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.nav_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
+
+
 }
