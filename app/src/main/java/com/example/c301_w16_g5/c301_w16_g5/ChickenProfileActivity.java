017eeb8df2d22ba5f36e82690b9e44978305382f
Robin <verleun@ualberta.ca>
2016-03-13 03:17:25 -0600

    Gui in progress for chicken profile. Not working yet. Compiles.

--- app/src/main/java/com/example/c301_w16_g5/c301_w16_g5/ChickenProfileActivity.java
+++ app/src/main/java/com/example/c301_w16_g5/c301_w16_g5/ChickenProfileActivity.java
@@ -1,33 +1,48 @@
 package com.example.c301_w16_g5.c301_w16_g5;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-public class ChickenProfileActivity extends AppCompatActivity {
+public class ChickenProfileActivity extends ChickBidActivity implements View.OnClickListener {
 
-    private Chicken currentChicken;
+    private static final int RESULT_LOAD_IMAGE = 1;
+    ImageView imageToUpload, downloadedImage;
+    Button bUploadImage;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chicken_profile);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        imageToUpload = (ImageView) findViewById(R.id.imageToUpload);
+
+        bUploadImage = (Button) findViewById(R.id.bUploadImage);
+
+        bUploadImage.setOnClickListener(this);
+
     }
 
+    @Override
+    public void onClick(View v){
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null){
+            Uri selectedImage = data.getData();
+            imageToUpload.setImageURI(selectedImage);
+        }
+    }
 }
