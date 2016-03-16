e9a75e41e97cf0eb8fb5c3324febc46bea52ab4c
Satyen Akolkar <s.akolkar3@gmail.com>
2016-03-15 19:59:11 -0600

    added view bids activity

---
+++ app/src/main/java/com/example/c301_w16_g5/c301_w16_g5/ViewBidsActivity.java
@@ -0,0 +1,23 @@
+package com.example.c301_w16_g5.c301_w16_g5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ViewBidsActivity extends ChickBidActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_bids);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.nav_toolbar);
+        setSupportActionBar(toolbar);
+
+        // show back arrow at top left
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+}
