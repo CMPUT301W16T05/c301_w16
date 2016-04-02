1b294989c38ed161924bd52b55df856a7602b8bc
Shahzeb Asif <asif.shahzeb@hotmail.com>
2016-04-02 14:26:45 -0600

    Fixed home button

--- app/src/main/java/com/example/c301_w16_g5/c301_w16_g5/ChickBidActivity.java
+++ app/src/main/java/com/example/c301_w16_g5/c301_w16_g5/ChickBidActivity.java
@@ -1,13 +1,10 @@
 package com.example.c301_w16_g5.c301_w16_g5;
 
 import android.content.Intent;
-import android.support.v4.app.NavUtils;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import java.util.Observer;
 
 /**
  * This activity is responsible for placing the toolbar at the top of every
@@ -32,18 +29,24 @@ public abstract class ChickBidActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem menuItem) {
+        Intent intent = null;
+
         super.onOptionsItemSelected(menuItem);
         switch (menuItem.getItemId()) {
             case android.R.id.home:
                 finish();
                 break;
             case R.id.home_button:
-                startActivity(new Intent(this, HomeActivity.class));
+                intent = new Intent(this, HomeActivity.class);
+                // 2016-04-02 http://stackoverflow.com/questions/11460896/button-to-go-back-to-mainactivity
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 break;
             case R.id.notifications_button:
-                startActivity(new Intent(this, NotificationsActivity.class));
+                intent = new Intent(this, NotificationsActivity.class);
                 break;
         }
+
+        startActivity(intent);
         return true;
     }
 }
