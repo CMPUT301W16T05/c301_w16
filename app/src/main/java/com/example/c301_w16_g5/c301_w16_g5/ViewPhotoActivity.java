9caac36ecc39dd17cbe80942523797359ecd41a6
Robin <verleun@ualberta.ca>
2016-04-04 10:29:45 -0600

    Can now delete a photo.

--- app/src/main/java/com/example/c301_w16_g5/c301_w16_g5/ViewPhotoActivity.java
+++ app/src/main/java/com/example/c301_w16_g5/c301_w16_g5/ViewPhotoActivity.java
@@ -2,10 +2,14 @@ package com.example.c301_w16_g5.c301_w16_g5;
 
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * <code>ViewPhotoActivity</code> provides a view of the photo attached to a
@@ -23,6 +27,7 @@ public class ViewPhotoActivity extends ChickBidActivity {
     ImageView imageChicken;
 
     Chicken currentChicken;
+    User current_user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +39,16 @@ public class ViewPhotoActivity extends ChickBidActivity {
         // show back arrow at top left
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+
         currentChicken = ChickBidsApplication.getChickenController().getCurrentChicken();
+        current_user = ChickBidsApplication.getUserController().getCurrentUser();
 
         imageChicken = (ImageView) findViewById(R.id.imageChicken);
         name = (TextView) findViewById(R.id.name);
+
+        if(currentChicken.getOwnerUsername().equals(current_user.getUsername()) && currentChicken.getPhoto() != null) {
+            registerForContextMenu(imageChicken);
+        }
 
         name.setText(currentChicken.getName());
         if(currentChicken.getPhoto() != null){
@@ -45,5 +56,32 @@ public class ViewPhotoActivity extends ChickBidActivity {
         } else {
             imageChicken.setVisibility(View.INVISIBLE);
         }
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.item_delete, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.delete:
+                if (ChickBidsApplication.getSearchController().checkOnline()) {
+                    if (currentChicken.getOwnerUsername().equals(current_user.getUsername())) {
+                        imageChicken.setImageDrawable(null);
+                        currentChicken.clearPhoto();
+                        ChickBidsApplication.getChickenController().updateChickenForMe(currentChicken);
+                    } else {
+                        Toast.makeText(this, "Cannot delete a borrowed chicken image.", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(this, "Cannot delete a chicken image offline.", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+        }
+        return true;
     }
 }
